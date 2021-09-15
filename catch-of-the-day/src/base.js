357a191c2890ca09c60fc8d29be52acc814684c7
Erin <erinllouise3@gmail.com>
2021-09-16 11:50:18 +1200

    authentication fb and gh

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase'
 import Rebase from 're-base'
 
-const fireBaseApp = firebase.initializeApp({
+const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyDFvA5uUe1a_VljXwt4sIaL8LWyws4ihsM",
   authDomain: "catch-of-the-day-b44a2.firebaseapp.com",
   databaseURL: "https://catch-of-the-day-b44a2-default-rtdb.firebaseio.com"
 })
 
-const base = Rebase.createClass(fireBaseApp.database())
+const base = Rebase.createClass(firebaseApp.database())
 
-export { fireBaseApp }
+export { firebaseApp }
 
 export default base
